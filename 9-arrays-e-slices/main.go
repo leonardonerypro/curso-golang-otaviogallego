0b9f812310fb31ca0d0ce20558a11e263beec1ea
Leonardo Nery <leonardonerypro@gmail.com>
2025-01-06 11:51:14 -0300

    9-atualizacao

--- 9-arrays-e-slices/main.go
+++ 9-arrays-e-slices/main.go
@@ -36,4 +36,21 @@ func main() {
 
 	array2[0] = "Posição alterada"
 	fmt.Println(slice2)
+
+	// Arreys Internos
+	slice3 := make([]float32, 10, 11)
+	fmt.Println(slice3)
+
+	slice3 = append(slice3, 5)
+	slice3 = append(slice3, 6)
+
+	fmt.Println(len(slice3)) // tamanho
+	fmt.Println(cap(slice3)) // capacidade
+	fmt.Println(slice3)
+
+	slice4 := make([]float32, 5)
+	fmt.Println(slice4)
+	slice4 = append(slice4, 10)
+	fmt.Println(len(slice4))
+	fmt.Println(cap(slice4))
 }
